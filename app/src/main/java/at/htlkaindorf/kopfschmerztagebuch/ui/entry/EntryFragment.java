c5c2c2ad7cc030853f3daa5e702674e8972053af
Gl3mi <klemenspechmann@gmail.com>
2022-07-11 11:05:20 +0200

    Initial commit

---
+++ app/src/main/java/at/htlkaindorf/kopfschmerztagebuch/ui/entry/EntryFragment.java
@@ -0,0 +1,37 @@
+package at.htlkaindorf.kopfschmerztagebuch.ui.entry;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import at.htlkaindorf.kopfschmerztagebuch.databinding.FragmentEntryBinding;
+
+public class EntryFragment extends Fragment {
+
+    private FragmentEntryBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        EntryViewModel entryViewModel =
+                new ViewModelProvider(this).get(EntryViewModel.class);
+
+        binding = FragmentEntryBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textEntry;
+        entryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/at/htlkaindorf/kopfschmerztagebuch/ui/entry/EntryViewModel.java
@@ -0,0 +1,19 @@
+package at.htlkaindorf.kopfschmerztagebuch.ui.entry;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class EntryViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public EntryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is entry fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
