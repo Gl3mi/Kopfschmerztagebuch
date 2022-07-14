8dc9eefa23cd15642b117531fd774371ad97a8d1
Desco24 <alexander.descovich-jentsch@gmx.at>
2022-07-14 15:00:57 +0200

    Calenndar

--- app/src/main/java/at/htlkaindorf/kopfschmerztagebuch/ui/calendar/CalendarFragment.java
+++ app/src/main/java/at/htlkaindorf/kopfschmerztagebuch/ui/calendar/CalendarFragment.java
@@ -1,21 +1,45 @@
 package at.htlkaindorf.kopfschmerztagebuch.ui.calendar;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.xml.sax.XMLReader;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 import at.htlkaindorf.kopfschmerztagebuch.databinding.FragmentCalendarBinding;
 
-public class CalendarFragment extends Fragment {
+public class CalendarFragment extends Fragment{
 
     private FragmentCalendarBinding binding;
+    private CalendarView calendar;
+    private TextView chosenDate;
+    private TextView connector;
+    private float[] datePosition;
+    private XMLReader reader;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         CalendarViewModel calendarViewModel =
@@ -24,8 +48,18 @@ public class CalendarFragment extends Fragment {
         binding = FragmentCalendarBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textCalendar;
-        calendarViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        chosenDate = binding.date;
+        chosenDate.setText(getTodaysDate());
+        calendar = binding.calendarView;
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                chosenDate.setText("" + dayOfMonth);
+            }
+        });
+
+        final TextView textView = binding.date;
+        //calendarViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
@@ -33,5 +67,12 @@ public class CalendarFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private String getTodaysDate()
+    {
+        Calendar cal = Calendar.getInstance();
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return "" + day;
     }
 }
