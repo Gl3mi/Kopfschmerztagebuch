01afb79cd1fdf7c6e2b2adbcbe44759702253750
Desco24 <alexander.descovich-jentsch@gmx.at>
2022-07-19 15:40:15 +0200

    Summary 2

--- app/src/main/java/at/htlkaindorf/kopfschmerztagebuch/ui/summary/SummaryFragment.java
+++ app/src/main/java/at/htlkaindorf/kopfschmerztagebuch/ui/summary/SummaryFragment.java
@@ -1,5 +1,6 @@
 package at.htlkaindorf.kopfschmerztagebuch.ui.summary;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +11,16 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+
 import at.htlkaindorf.kopfschmerztagebuch.databinding.FragmentSummaryBinding;
 
 public class SummaryFragment extends Fragment {
 
     private FragmentSummaryBinding binding;
+    TextView tvR, tvPython, tvCPP, tvJava;
+    PieChart pieChart;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,8 +30,40 @@ public class SummaryFragment extends Fragment {
         binding = FragmentSummaryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSummary;
-        summaryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        tvR = binding.tvR;
+        tvPython = binding.tvPython;
+        tvCPP = binding.tvCPP;
+        tvJava = binding.tvJava;
+        pieChart = binding.piechart;
+
+        tvPython.setText(Integer.toString(40));
+        tvCPP.setText(Integer.toString(10));
+        tvJava.setText(Integer.toString(50));
+
+        pieChart.addPieSlice(
+                new PieModel(
+                        "R",
+                        Integer.parseInt(tvR.getText().toString()),
+                        Color.parseColor("#FFA726")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Python",
+                        Integer.parseInt(tvPython.getText().toString()),
+                        Color.parseColor("#66BB6A")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "C++",
+                        Integer.parseInt(tvCPP.getText().toString()),
+                        Color.parseColor("#EF5350")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Java",
+                        Integer.parseInt(tvJava.getText().toString()),
+                        Color.parseColor("#29B6F6")));
+
+        pieChart.startAnimation();
+        //final TextView textView = binding.textView;
+        //summaryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
